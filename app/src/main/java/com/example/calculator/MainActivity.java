46432cb924cc7c878a1681b51e98ff7d770be5c8
Korbo <korbo1995@yahoo.gr>
2020-11-08 20:43:21 +0200

    aaaa

--- app/src/main/java/com/example/calculator/MainActivity.java
+++ app/src/main/java/com/example/calculator/MainActivity.java
@@ -48,6 +48,7 @@ public class MainActivity extends AppCompatActivity {
 
         //noinspection SimplifiableIfStatement
         if (id == R.id.action_settings) {
+            //aaaaaaaaaaaaa
             return true;
         }
 
